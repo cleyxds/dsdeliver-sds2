e2b783d5009c50d348ccb87416c28983a25a3cde
mago <cleysonn@hotmail.com.br>
2021-01-07 17:17:26 -0300

    Project created

---
+++ backend/src/test/java/com/cleyxds/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleyxds.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
