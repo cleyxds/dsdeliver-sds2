ad33b68485da6e05fd7e3b489377790ebebf16fe
mago <cleysonn@hotmail.com.br>
2021-01-07 21:40:35 -0300

    GET /orders

---
+++ backend/src/main/java/com/cleyxds/dsdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.cleyxds.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cleyxds.dsdeliver.dto.OrderDTO;
+import com.cleyxds.dsdeliver.entities.Order;
+import com.cleyxds.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
