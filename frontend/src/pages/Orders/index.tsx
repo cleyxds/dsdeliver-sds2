f4e9367a059a52bb9af29ef5ba730680f9791e07
mago <cleysonn@hotmail.com.br>
2021-01-10 16:08:27 -0300

    Search order location

--- frontend/src/pages/Orders/index.tsx
+++ frontend/src/pages/Orders/index.tsx
@@ -2,11 +2,13 @@ import './styles.css';
 import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
 import { useEffect, useState } from 'react';
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../../services/api';
+import OrderLocation from './OrderLocation';
 
 function Orders() {
 	const [products, setProducts] = useState<Product[]>([]);
+	const [orderLocation, setOrderLocation] = useState<OrderLocationData>()
 	
 	useEffect(() => {
 		fetchProducts()
@@ -18,6 +20,7 @@ function Orders() {
 		<div className="orders-container">
 			<StepsHeader />
 			<ProductsList products={products}/>
+			<OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
 		</div>
 	)
 }
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const API_URL = 'http://localhost:8080';
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
   return axios.get(`${API_URL}/products`);
+}
+
+export function fetchLocalMapBox( local:string ) {
+  return axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 }
