e3374f8321e0dcf1e1c3999d056bf4c74366c9d8
mago <cleysonn@hotmail.com.br>
2021-01-10 17:55:36 -0300

    Finish frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css';
 import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return <Routes />
+  return (
+  <>
+    <ToastContainer />
+    <Routes />
+  </>
+  )
 }
 
 export default App;
