21ec673b725a4c9e5802754ebf64e4745d08c5d5
mago <cleysonn@hotmail.com.br>
2021-01-09 22:41:52 -0300

    API and order page

--- frontend/src/pages/Orders/index.tsx
+++ frontend/src/pages/Orders/index.tsx
@@ -1,10 +1,23 @@
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../../services/api';
 
 function Orders() {
+	const [products, setProducts] = useState<Product[]>([]);
+	
+	useEffect(() => {
+		fetchProducts()
+			.then(response => setProducts(response.data))
+			.catch(error => console.log(error));
+	},[]);
+
 	return (
 		<div className="orders-container">
 			<StepsHeader />
+			<ProductsList products={products}/>
 		</div>
 	)
 }
